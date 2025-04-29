dfd6d90f05021da5e85525772dddfe7876455432
👻XieJinPeng <xiejinpeng@1871768705682871.onaliyun.com>
2025-04-29 10:37:00 +0800

    feat: add rollup-plugin-visualizer for bundle analysis

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+
   build: {
-    target: 'esnext'
+    target: 'esnext',
+    rollupOptions: {
+      plugins: [visualizer()]
+    },
   }
 })
